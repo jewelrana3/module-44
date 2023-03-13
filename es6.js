e9d7e0feee0afee1c63bde923ac4941a485a82bc
jewelrana3 <juyelrana7752@gmail.com>
2023-03-13 08:38:04 +0600

    test

---
+++ es6.js
@@ -0,0 +1,10 @@
+const number = [3, 44, 543, 5, 67, 76];
+const student = {
+    name: 'juyel',
+    age: 33,
+    address: 'bademaju',
+    movies: ['king khan', 'my name is khan']
+}
+
+const about = `my name is ${student.name} and age ${student.age} and movies ${number[2]}`
+console.log(about)
